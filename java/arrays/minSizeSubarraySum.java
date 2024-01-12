87c2add7a8b83ef6f0a83a1ad801371826459cbe
V-Ravi-Teja <64315770+V-Ravi-Teja@users.noreply.github.com>
2024-01-12 11:22:16 +0530

    arrays updated

--- java/arrays/minSizeSubarraySum.java
+++ java/arrays/minSizeSubarraySum.java
@@ -2,7 +2,8 @@ public class minSizeSubarraySum {
     /*
         https://leetcode.com/problems/minimum-size-subarray-sum/description/
 
-        Q) Given an array of positive integers nums and a positive integer target, return the minimal length of a Subarray whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+        Q) Given an array of positive integers nums and a positive integer target, return the minimal length of a
+        Subarray whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
 
         Example 1:`
         `Input: target = 7, nums = [2,3,1,2,4,3]`
